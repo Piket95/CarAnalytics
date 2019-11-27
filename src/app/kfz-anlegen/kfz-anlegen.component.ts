00d80f6d6ec7cb10bbd766e5dca4646ee1f7bdd3
Philipp Dalheimer <philippdalheimer@gmail.com>
2019-11-27 12:21:26 +0100

    + Komponente für Seite erstellt

---
+++ src/app/kfz-anlegen/kfz-anlegen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kfz-anlegen',
+  templateUrl: './kfz-anlegen.component.html',
+  styleUrls: ['./kfz-anlegen.component.css']
+})
+export class KfzAnlegenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
