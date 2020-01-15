2ce8b8b22b2ce12c41cc79aa0e1bc4feb27734ae
Dennis Adam <dennis.adam95@gmail.com>
2020-01-15 07:06:05 +0100

    * HTML des Profil editieren Formulars angepasst + Automatisches ausfüllen von schon vorhandenen Werten des Profils ins Formular + API Verbindung implementiert 	-> Buggy, da sich das Profil nicht ändern lässt 		-> Passwort kann falsch angegeben werden 		-> Bei richtiger Passworteingabe, kommen trotzdem die alten Werte zurück

--- src/app/edit-user-profile/edit-user-profile.component.ts
+++ src/app/edit-user-profile/edit-user-profile.component.ts
@@ -1,7 +1,8 @@
+import { AppComponent } from './../app.component';
+import { HttpParams, HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-user-profile',
@@ -11,38 +12,86 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class EditUserProfileComponent implements OnInit {
 
   public userData;
-  form: FormGroup;
+
+  firstname: string;
+  lastname: string;
+  favBrand: string;
+  favCar: string;
 
   constructor(
-    private fb: FormBuilder,
     private router: Router,
-    private toastr: ToastrService
-  ) {
+    private toastr: ToastrService,
+    private appcomponent: AppComponent,
+    private http: HttpClient
+  ) {  }
+
+  ngOnInit() {
     if (localStorage.hasOwnProperty('currentUser')) {
       this.userData = JSON.parse(localStorage.getItem('currentUser'));
     }
 
-    this.createForm();
+    this.firstname = (this.userData.firstname === null) ? '' : this.userData.firstname;
+    this.lastname = (this.userData.lastname === null) ? '' : this.userData.lastname;
+    this.favBrand = (this.userData.fav_brand === null) ? '' : this.userData.fav_brand;
+    this.favCar = (this.userData.fav_car === null) ? '' : this.userData.fav_car;
   }
 
-  ngOnInit() {
+  onClickSubmit(formData) {
 
-  }
-
-  createForm() {
-    this.form = this.fb.group({
-      name: ['', Validators.required]
-    });
-  }
-
-  onClickSubmit() {
-    //Beispiel in KFZ-Anlegen
-
-    //TODO: Daten an API wegschicken mit Password
     //TODO: Passwortübereinstimmung überprüfen, falls gesetzt!
 
-    this.toastr.success('Profil wurde erfolgreich aktualisiert!');
-    this.router.navigate(['/profile']);
+    if (formData.password !== '') {
+
+      const body = new HttpParams()
+      .set('api_key', this.appcomponent.api_key)
+      .set('password', formData.password);
+
+      if (formData.firstname !== '') {
+        body.append('firstname', formData.firstname);
+      }
+
+      if (formData.lastname !== '') {
+        body.append('lastname', formData.lastname);
+      }
+
+      if (formData.favBrand !== '') {
+        body.append('fav_brand', formData.favBrand);
+      }
+
+      if (formData.favCar !== '') {
+        body.append('fav_car', formData.favCar);
+      }
+
+      //Passwort ändern (Überprüfen ob beide gleich sind!!)
+      if (formData.newPW1 !== '' && formData.newPW2 !== '') {
+        if (formData.newPW1 === formData.newPW2) {
+          //TODO: bleibt das auf "password_old" stehen oder wird das in new geändert?
+          body.append('password_old', formData.newPW2);
+        }
+      } else if ((formData.newPW1 !== '' && formData.newPW2 === '') || (formData.newPW1 === '' && formData.newPW2 !== '')) {
+        this.toastr.error(
+          'Bitte füllen Sie beide Passwortfelder aus, wenn Sie Ihr Passwort ändern möchten!',
+          'Bitte alle Passwortfelder ausfüllen'
+        );
+      }
+
+
+      //TODO: Daten an API wegschicken mit Password
+
+      this.http.post('https://api.philippdalheimer.de/request/user/get/' + this.userData.id, body)
+      .subscribe(data => {
+        console.log(data);
+
+        if (data['success'] == true) {
+          localStorage.setItem('currentUser', JSON.stringify(data['user']));
+          this.toastr.success('Profil wurde erfolgreich aktualisiert!');
+          this.router.navigate(['/profile']);
+        }
+      });
+
+    } else {
+      this.toastr.error('Bitte geben Sie Ihr aktuelles Passwort an, um die Profilaktualisierung abzuschließen!', 'Passwort fehlt');
+    }
   }
 
 }
